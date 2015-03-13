899ee8e6128060ee5dea4494affa88fdc14098b5
Craig Harman <craig@craigharman.net>
2015-03-13 16:05:47 -0400

    Added documentation for concrete.inspect module

--- regenerate_docs.sh
+++ regenerate_docs.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+pdoc --html --overwrite concrete.inspect
 pdoc --html --overwrite concrete.util
 pdoc --html --overwrite concrete.validate
-python pdoc_whitelisted_submodules.py concrete.util concrete.validate > concrete/index.html
+python pdoc_whitelisted_submodules.py concrete.inspect concrete.util concrete.validate > concrete/index.html
 
 rm -rf util
 mv concrete/* .
