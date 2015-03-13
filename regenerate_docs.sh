8f0dc36c818258daf83c3cb1d25d7beeab602247
Craig Harman <craig@craigharman.net>
2015-03-13 14:49:45 -0400

    Fixed regenerate_docs.sh permission issue when regenerating docs

--- regenerate_docs.sh
+++ regenerate_docs.sh
@@ -2,5 +2,7 @@
 pdoc --html --overwrite concrete.util
 pdoc --html --overwrite concrete.validate
 python pdoc_whitelisted_submodules.py concrete.util concrete.validate > concrete/index.html
+
+rm -rf util
 mv concrete/* .
 rmdir concrete
